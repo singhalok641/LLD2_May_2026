42efa49f267b84a17d7fbb718e07a16d1afb1ef9
Alok Singh <singhalok641@gmail.com>
2026-06-09 22:47:07 +0200

    flyweight design pattern

--- src/main/java/org/example/designpatterns/flyweight/BulletTypeFactory.java
+++ src/main/java/org/example/designpatterns/flyweight/BulletTypeFactory.java
@@ -17,5 +17,27 @@ import java.util.Map;
  * copy of the image and shared attributes (~1 KB each).
  */
 public class BulletTypeFactory {
+    private static final Map<String, BulletType> CACHE = new HashMap<>();
 
+    public static BulletType getBulletType(String name) {
+        if (CACHE.containsKey(name)) {
+            return CACHE.get(name); // reuse the exisiting
+        }
+
+        // First time - create the heavy object
+        BulletType bulletType = createBulletType(name);
+        CACHE.put(name, bulletType); // cache it for reuse
+        return bulletType;
+    }
+
+    private static BulletType createBulletType(String name) {
+        return switch (name) {
+            case "AKM" -> new BulletType("GOLD", 8.0, 3.9, 48,
+                    400, new byte[1024]);
+            case "M416" -> new BulletType("SILVER", 8.0, 3.9, 48,
+                    400, new byte[1024]);
+            default -> new BulletType("GRAY", 7.0, 3.0, 30,
+                    300, new byte[1024]);
+        };
+    }
 }
